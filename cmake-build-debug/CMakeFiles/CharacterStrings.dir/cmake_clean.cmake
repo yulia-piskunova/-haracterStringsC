b733fcecdb186fa367072ea2c505468327de4206
yulia-piskunova <y.piskunova@tapston.com>
2020-10-18 13:35:48 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/CharacterStrings.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CharacterStrings.dir/main.c.o"
+  "CharacterStrings"
+  "CharacterStrings.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CharacterStrings.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CharacterStrings.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CharacterStrings.
+# This may be replaced when dependencies are built.
