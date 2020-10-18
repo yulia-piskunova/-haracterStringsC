2e6bb69b2d3f80688e5990d7c2ab4abc93895065
yulia-piskunova <y.piskunova@tapston.com>
2020-10-18 13:44:45 +0300

    string input method

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    char s[100];
+    int i, id, min, count, len;
+
+    gets(s);
+    len = strlen(s);
+    min = strlen(s);
+    id = 0;
+    count = 0;
+
+    for (i = 0 ; i < len ; i++)
+        if (s[i] != ' ') count += 1;
+        else {
+            if (count < min) {
+
+            }
+            count = 0;
+        }
+
+    printf("\n");
 }
